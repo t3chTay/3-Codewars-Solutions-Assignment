d2ac006c9a27a28df51a48eee4686c9a86e0f3fc
t3chTay <tayjohnson101@icloud.com>
2025-12-10 15:59:14 -0500

    Challenge's prompt/description

--- vowel_search.js
+++ vowel_search.js
@@ -1,4 +1,9 @@
 // Is there a vowel in there?
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+// If they are, change the array value to a string of that vowel.
+
+// input [100,100,116,105,117,121]=>[100,100,116,"i","u",121] output Return the resulting array.
 
 // ASCII Vowel Codes
 // a = 97
